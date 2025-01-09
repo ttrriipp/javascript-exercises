99aaa03441ab5ead89b4c3cc31d29e226028136c
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-01-09 14:51:19 +0800

    exercise 05 done

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(first, last) {
+    sum = 0;
 
+    if (first < 0 || last < 0 ||
+        typeof first != "number" ||
+        typeof last != "number" ||
+        first != Math.trunc(first) ||
+        last != Math.trunc(last)) {
+        return "ERROR"; 
+    } else if (last >= first) {
+        for (let i = first; i <= last; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = last; i <= first; i++) {
+            sum += i;
+        }
+    }
+    return sum;
 };
+
+console.log(sumAll(123, 1));
 
 // Do not edit below this line
 module.exports = sumAll;
