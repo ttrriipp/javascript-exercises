e38dec3363d76cb6a17de1e9f092ce1c31288a8b
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-01-09 11:19:24 +0800

    exercise 02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let repeatString = "";
+    for (let i = 0; i < num; i++) {
+        repeatString += string;
+    }
+    if (num < 0) return "ERROR"
+    return repeatString;
 };
 
 // Do not edit below this line
