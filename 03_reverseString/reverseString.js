4c06843cefe7194203ef7e76255e938c832bf318
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-01-09 11:33:43 +0800

    exercise 03 done

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reversedString = "";
+    const characters = string.split("");
+    for (let i = string.length - 1; i >= 0; i--) {
+        reversedString += characters[i];
+    }
+    return reversedString;
 };
 
 // Do not edit below this line
