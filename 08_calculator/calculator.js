69e2fbe1978da517be81371d4527ce069c248ee9
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-01-17 12:56:05 +0800

    exercise 08 done

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,36 @@
-const add = function() {
-	
+const add = function(...num) {
+  return num.reduce((total, item) => {
+    return total + item;
+  }, 0);
 };
 
-const subtract = function() {
-	
+const subtract = function(...num) {
+  return num[0] - num[1];	
 };
 
-const sum = function() {
-	
+const sum = function(num) {
+  return num.reduce((total, item) =>  total + item, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(num) {
+  return num.reduce((total, item) =>  total * item, 1);
 };
 
-const power = function() {
-	
+const power = function(...num) {
+  return num.reduce((total, item) => {
+    const powerValue = num[0];
+    for (let i = 1 ; i < item; i++) {
+      total *= powerValue;
+    }
+    return total;
+  });
 };
 
-const factorial = function() {
+const factorial = function(num) {
+  for (let i = num - 1; i > 0; i--) {
+    num *= i;
+  }
+  return num === 0 ? 1 : num;
 	
 };
 
