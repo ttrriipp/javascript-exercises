54a7b34b9d4d2461f9847d5992e32b8b7ac93c81
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-01-17 13:21:01 +0800

    exercise 09 done

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,16 @@
-const palindromes = function () {
+//racecar
+//single words
+// punctuation
+//upper-case letters
+//numbers
+//multiple words
 
+const palindromes = function (str) {
+    return Array.from(str).reverse().filter(char => /[^\W_]/.test(char)).join('').toLowerCase() == [...str].filter(char => /[^\W_]/.test(char)).join('').toLowerCase();
 };
+
+console.log(palindromes("Racecar!"));
+
 
 // Do not edit below this line
 module.exports = palindromes;
