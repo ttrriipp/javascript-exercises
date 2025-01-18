db03189e0182f63cbac5cf888727944c4a9b565a
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-01-18 09:55:31 +0800

    exercise 11 done

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,17 @@
-const getTheTitles = function() {
+const books = [
+      {
+        title: 'Book',
+        author: 'Name'
+      },
+      {
+        title: 'Book2',
+        author: 'Name2'
+      }
+    ]
 
+const getTheTitles = function(array) {
+    return array.map(item => item.title);
 };
-
+console.log(getTheTitles(books));
 // Do not edit below this line
 module.exports = getTheTitles;
