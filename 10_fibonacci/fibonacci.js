a799d0ed499f8a6677a9051a6b733579e302d514
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-01-18 09:42:38 +0800

    exercise 10 done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,20 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) {
+        return "OOPS";
+    }
+    if (num == 1){
+        return 1;
+    }
+    num = Number(num);
+    let sum = 0;
+    let F = [0, 1];
+    for (let i = 1; i < num; i++) {
+        sum = F[0] + F[1]; //1 , 2, 3
+        F[0] = F[1]; // 1, 1
+        F[1] = sum; // 1, 2
+    }
+    return sum;
 };
-
+console.log(fibonacci(0));
 // Do not edit below this line
 module.exports = fibonacci;
